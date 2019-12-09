3fdafa7bd6d084cf049f88ede1c9de88b68cb1f2
Nakiface <BernhardSchmitt@googlemail.com>
2019-12-09 14:33:46 +0100

    Projektdateien hinzufügen.

---
+++ Schräger Wurf/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace Schräger_Wurf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ev0textBox = new System.Windows.Forms.TextBox();
+            this.eaWinkeltextBox = new System.Windows.Forms.TextBox();
+            this.awurfweitetextBox = new System.Windows.Forms.TextBox();
+            this.amaxhoehetextBox = new System.Windows.Forms.TextBox();
+            this.azeittextBox = new System.Windows.Forms.TextBox();
+            this.Überschriftlabel = new System.Windows.Forms.Label();
+            this.azeitlabel = new System.Windows.Forms.Label();
+            this.ahoehelabel = new System.Windows.Forms.Label();
+            this.ausgabelabel = new System.Windows.Forms.Label();
+            this.awurfweitelabel = new System.Windows.Forms.Label();
+            this.eingabelabel = new System.Windows.Forms.Label();
+            this.aWinkellabel = new System.Windows.Forms.Label();
+            this.v0label = new System.Windows.Forms.Label();
+            this.Eingabepanel = new System.Windows.Forms.Panel();
+            this.eZeittextBox = new System.Windows.Forms.TextBox();
+            this.eZeitlabel = new System.Windows.Forms.Label();
+            this.eZeitcheckBox = new System.Windows.Forms.CheckBox();
+            this.apanel = new System.Windows.Forms.Panel();
+            this.aZeitpanel = new System.Windows.Forms.Panel();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.ageschwindigkeittextBox = new System.Windows.Forms.TextBox();
+            this.ahoehetextBox = new System.Windows.Forms.TextBox();
+            this.aentfehrnungtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Eingabepanel.SuspendLayout();
+            this.apanel.SuspendLayout();
+            this.aZeitpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ev0textBox
+            // 
+            this.ev0textBox.Location = new System.Drawing.Point(18, 57);
+            this.ev0textBox.Name = "ev0textBox";
+            this.ev0textBox.Size = new System.Drawing.Size(100, 22);
+            this.ev0textBox.TabIndex = 0;
+            // 
+            // eaWinkeltextBox
+            // 
+            this.eaWinkeltextBox.Location = new System.Drawing.Point(18, 103);
+            this.eaWinkeltextBox.Name = "eaWinkeltextBox";
+            this.eaWinkeltextBox.Size = new System.Drawing.Size(100, 22);
+            this.eaWinkeltextBox.TabIndex = 2;
+            // 
+            // awurfweitetextBox
+            // 
+            this.awurfweitetextBox.Location = new System.Drawing.Point(132, 33);
+            this.awurfweitetextBox.Name = "awurfweitetextBox";
+            this.awurfweitetextBox.Size = new System.Drawing.Size(100, 22);
+            this.awurfweitetextBox.TabIndex = 3;
+            // 
+            // amaxhoehetextBox
+            // 
+            this.amaxhoehetextBox.Location = new System.Drawing.Point(132, 61);
+            this.amaxhoehetextBox.Name = "amaxhoehetextBox";
+            this.amaxhoehetextBox.Size = new System.Drawing.Size(100, 22);
+            this.amaxhoehetextBox.TabIndex = 4;
+            // 
+            // azeittextBox
+            // 
+            this.azeittextBox.Location = new System.Drawing.Point(132, 89);
+            this.azeittextBox.Name = "azeittextBox";
+            this.azeittextBox.Size = new System.Drawing.Size(100, 22);
+            this.azeittextBox.TabIndex = 5;
+            // 
+            // Überschriftlabel
+            // 
+            this.Überschriftlabel.AutoSize = true;
+            this.Überschriftlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Überschriftlabel.Location = new System.Drawing.Point(62, 9);
+            this.Überschriftlabel.Name = "Überschriftlabel";
+            this.Überschriftlabel.Size = new System.Drawing.Size(208, 36);
+            this.Überschriftlabel.TabIndex = 6;
+            this.Überschriftlabel.Text = "Schräger Wurf";
+            // 
+            // azeitlabel
+            // 
+            this.azeitlabel.AutoSize = true;
+            this.azeitlabel.Location = new System.Drawing.Point(16, 92);
+            this.azeitlabel.Name = "azeitlabel";
+            this.azeitlabel.Size = new System.Drawing.Size(32, 17);
+            this.azeitlabel.TabIndex = 7;
+            this.azeitlabel.Text = "Zeit";
+            // 
+            // ahoehelabel
+            // 
+            this.ahoehelabel.AutoSize = true;
+            this.ahoehelabel.Location = new System.Drawing.Point(16, 64);
+            this.ahoehelabel.Name = "ahoehelabel";
+            this.ahoehelabel.Size = new System.Drawing.Size(71, 17);
+            this.ahoehelabel.TabIndex = 8;
+            this.ahoehelabel.Text = "max Höhe";
+            // 
+            // ausgabelabel
+            // 
+            this.ausgabelabel.AutoSize = true;
+            this.ausgabelabel.Location = new System.Drawing.Point(84, 10);
+            this.ausgabelabel.Name = "ausgabelabel";
+            this.ausgabelabel.Size = new System.Drawing.Size(64, 17);
+            this.ausgabelabel.TabIndex = 9;
+            this.ausgabelabel.Text = "Ausgabe";
+            // 
+            // awurfweitelabel
+            // 
+            this.awurfweitelabel.AutoSize = true;
+            this.awurfweitelabel.Location = new System.Drawing.Point(16, 36);
+            this.awurfweitelabel.Name = "awurfweitelabel";
+            this.awurfweitelabel.Size = new System.Drawing.Size(70, 17);
+            this.awurfweitelabel.TabIndex = 10;
+            this.awurfweitelabel.Text = "Wurfweite";
+            // 
+            // eingabelabel
+            // 
+            this.eingabelabel.AutoSize = true;
+            this.eingabelabel.Location = new System.Drawing.Point(83, 10);
+            this.eingabelabel.Name = "eingabelabel";
+            this.eingabelabel.Size = new System.Drawing.Size(60, 17);
+            this.eingabelabel.TabIndex = 11;
+            this.eingabelabel.Text = "Eingabe";
+            // 
+            // aWinkellabel
+            // 
+            this.aWinkellabel.AutoSize = true;
+            this.aWinkellabel.Location = new System.Drawing.Point(15, 83);
+            this.aWinkellabel.Name = "aWinkellabel";
+            this.aWinkellabel.Size = new System.Drawing.Size(89, 17);
+            this.aWinkellabel.TabIndex = 13;
+            this.aWinkellabel.Text = "Abwurfwinkel";
+            // 
+            // v0label
+            // 
+            this.v0label.AutoSize = true;
+            this.v0label.Location = new System.Drawing.Point(15, 37);
+            this.v0label.Name = "v0label";
+            this.v0label.Size = new System.Drawing.Size(169, 17);
+            this.v0label.TabIndex = 14;
+            this.v0label.Text = "Abwurfgeschwindigkeit v0";
+            // 
+            // Eingabepanel
+            // 
+            this.Eingabepanel.Controls.Add(this.startbutton);
+            this.Eingabepanel.Controls.Add(this.eZeittextBox);
+            this.Eingabepanel.Controls.Add(this.eZeitlabel);
+            this.Eingabepanel.Controls.Add(this.eZeitcheckBox);
+            this.Eingabepanel.Controls.Add(this.v0label);
+            this.Eingabepanel.Controls.Add(this.ev0textBox);
+            this.Eingabepanel.Controls.Add(this.aWinkellabel);
+            this.Eingabepanel.Controls.Add(this.eaWinkeltextBox);
+            this.Eingabepanel.Controls.Add(this.eingabelabel);
+            this.Eingabepanel.Location = new System.Drawing.Point(28, 57);
+            this.Eingabepanel.Name = "Eingabepanel";
+            this.Eingabepanel.Size = new System.Drawing.Size(214, 258);
+            this.Eingabepanel.TabIndex = 15;
+            // 
+            // eZeittextBox
+            // 
+            this.eZeittextBox.Location = new System.Drawing.Point(18, 149);
+            this.eZeittextBox.Name = "eZeittextBox";
+            this.eZeittextBox.Size = new System.Drawing.Size(100, 22);
+            this.eZeittextBox.TabIndex = 17;
+            // 
+            // eZeitlabel
+            // 
+            this.eZeitlabel.AutoSize = true;
+            this.eZeitlabel.Location = new System.Drawing.Point(15, 129);
+            this.eZeitlabel.Name = "eZeitlabel";
+            this.eZeitlabel.Size = new System.Drawing.Size(32, 17);
+            this.eZeitlabel.TabIndex = 16;
+            this.eZeitlabel.Text = "Zeit";
+            // 
+            // eZeitcheckBox
+            // 
+            this.eZeitcheckBox.AutoSize = true;
+            this.eZeitcheckBox.Location = new System.Drawing.Point(18, 183);
+            this.eZeitcheckBox.Name = "eZeitcheckBox";
+            this.eZeitcheckBox.Size = new System.Drawing.Size(110, 21);
+            this.eZeitcheckBox.TabIndex = 15;
+            this.eZeitcheckBox.Text = "Eingabe Zeit";
+            this.eZeitcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // apanel
+            // 
+            this.apanel.Controls.Add(this.ausgabelabel);
+            this.apanel.Controls.Add(this.awurfweitetextBox);
+            this.apanel.Controls.Add(this.amaxhoehetextBox);
+            this.apanel.Controls.Add(this.awurfweitelabel);
+            this.apanel.Controls.Add(this.azeittextBox);
+            this.apanel.Controls.Add(this.azeitlabel);
+            this.apanel.Controls.Add(this.ahoehelabel);
+            this.apanel.Location = new System.Drawing.Point(264, 57);
+            this.apanel.Name = "apanel";
+            this.apanel.Size = new System.Drawing.Size(260, 132);
+            this.apanel.TabIndex = 16;
+            // 
+            // aZeitpanel
+            // 
+            this.aZeitpanel.Controls.Add(this.label2);
+            this.aZeitpanel.Controls.Add(this.label3);
+            this.aZeitpanel.Controls.Add(this.label4);
+            this.aZeitpanel.Controls.Add(this.ageschwindigkeittextBox);
+            this.aZeitpanel.Controls.Add(this.ahoehetextBox);
+            this.aZeitpanel.Controls.Add(this.aentfehrnungtextBox);
+            this.aZeitpanel.Location = new System.Drawing.Point(264, 195);
+            this.aZeitpanel.Name = "aZeitpanel";
+            this.aZeitpanel.Size = new System.Drawing.Size(260, 120);
+            this.aZeitpanel.TabIndex = 17;
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(68, 217);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 18;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // ageschwindigkeittextBox
+            // 
+            this.ageschwindigkeittextBox.Location = new System.Drawing.Point(132, 20);
+            this.ageschwindigkeittextBox.Name = "ageschwindigkeittextBox";
+            this.ageschwindigkeittextBox.Size = new System.Drawing.Size(100, 22);
+            this.ageschwindigkeittextBox.TabIndex = 11;
+            // 
+            // ahoehetextBox
+            // 
+            this.ahoehetextBox.Location = new System.Drawing.Point(132, 48);
+            this.ahoehetextBox.Name = "ahoehetextBox";
+            this.ahoehetextBox.Size = new System.Drawing.Size(100, 22);
+            this.ahoehetextBox.TabIndex = 12;
+            // 
+            // aentfehrnungtextBox
+            // 
+            this.aentfehrnungtextBox.Location = new System.Drawing.Point(132, 76);
+            this.aentfehrnungtextBox.Name = "aentfehrnungtextBox";
+            this.aentfehrnungtextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.aentfehrnungtextBox.Size = new System.Drawing.Size(100, 22);
+            this.aentfehrnungtextBox.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Entfehrnung";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Höhe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Geschwindigkeit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aZeitpanel);
+            this.Controls.Add(this.apanel);
+            this.Controls.Add(this.Eingabepanel);
+            this.Controls.Add(this.Überschriftlabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Eingabepanel.ResumeLayout(false);
+            this.Eingabepanel.PerformLayout();
+            this.apanel.ResumeLayout(false);
+            this.apanel.PerformLayout();
+            this.aZeitpanel.ResumeLayout(false);
+            this.aZeitpanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ev0textBox;
+        private System.Windows.Forms.TextBox eaWinkeltextBox;
+        private System.Windows.Forms.TextBox awurfweitetextBox;
+        private System.Windows.Forms.TextBox amaxhoehetextBox;
+        private System.Windows.Forms.TextBox azeittextBox;
+        private System.Windows.Forms.Label Überschriftlabel;
+        private System.Windows.Forms.Label azeitlabel;
+        private System.Windows.Forms.Label ahoehelabel;
+        private System.Windows.Forms.Label ausgabelabel;
+        private System.Windows.Forms.Label awurfweitelabel;
+        private System.Windows.Forms.Label eingabelabel;
+        private System.Windows.Forms.Label aWinkellabel;
+        private System.Windows.Forms.Label v0label;
+        private System.Windows.Forms.Panel Eingabepanel;
+        private System.Windows.Forms.TextBox eZeittextBox;
+        private System.Windows.Forms.Label eZeitlabel;
+        private System.Windows.Forms.CheckBox eZeitcheckBox;
+        private System.Windows.Forms.Panel apanel;
+        private System.Windows.Forms.Panel aZeitpanel;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.TextBox ageschwindigkeittextBox;
+        private System.Windows.Forms.TextBox ahoehetextBox;
+        private System.Windows.Forms.TextBox aentfehrnungtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
